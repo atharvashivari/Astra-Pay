a93892b21743c89c237650a225a9642b1eea22f4
Atharva Manish Shivari <atharvashivari2558@gmail.com>
2026-03-20 14:51:25 +0530

    test: remove unused and duplicate imports in EndToEndTransferTest

--- backend/src/test/java/com/astrapay/integration/EndToEndTransferTest.java
+++ backend/src/test/java/com/astrapay/integration/EndToEndTransferTest.java
@@ -17,10 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
 import java.math.BigDecimal;
 import java.util.UUID;
